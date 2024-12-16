957191cd95b34525f0ded6436ee730e1f9e4c3da
jtavora <jtavora09@gmail.com>
2024-12-15 23:34:08 -0300

    Ok

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::prefix('auth')->group(function () {
     Route::post('/login', [LoginController::class, 'login']);
     Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
